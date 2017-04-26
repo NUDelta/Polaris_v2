ba391a5a0c2e2950a4f79ac0124a314f42a25d69
sameersrivastava <sameer.srivastava95@gmail.com>
2017-04-25 22:48:38 -0500

    Added react components like Bomani did.

---
+++ imports/ui/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+ 
+ 
+// App component - represents the whole app
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>APP</h1>
+      </div>
+    );
+  }
+}
+
+export default App; 
