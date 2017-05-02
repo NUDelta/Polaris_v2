db05e1fb4a2a53f15a3b73fd35a56303caafb800
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-05-02 10:21:57 -0500

    Sets up basic server endpoint for getting profile details in preparation for the sidebar

--- server/main.js
+++ server/main.js
@@ -5,6 +5,22 @@ import '../imports/api/knowledge-representation/knowledge-representation.js';
 
 import '/imports/startup/server';
 
+WebApp.connectHandlers.use('/goals', (req, res, next) => {
+	let targetID = req.query.id;
+	let responseObj = {error: "No matched users"};
+
+	if (targetID) {
+		let matchedUsers = Meteor.users.find({_id: targetID}).fetch();
+		
+		if (matchedUsers.length > 0) {
+			responseObj = matchedUsers[0].profile;
+		}
+	}
+
+	res.writeHead(200);
+	res.end(JSON.stringify(responseObj));
+});
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
